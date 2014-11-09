0c84d67adb1bf6244ef455791810a01250861a8c
miguel michelson <miguelmichelson@gmail.com>
2014-11-09 02:17:34 -0300

    initial gh-pages commit

---
+++ javascripts/specs/editor.js
@@ -0,0 +1,38 @@
+(function() {
+  window.editor = new Editor.MainEditor({
+    upload_url: "/images.json",
+    el: "#editor1"
+  });
+
+  window.editor.start();
+
+  QUnit.test("should initialize editor", function(assert) {
+    return assert.ok(_.isObject(window.editor), "Passed!");
+  });
+
+  QUnit.test("should init editor defaults", function(assert) {
+    assert.ok(!_.isEmpty(window.editor.title_placeholder), "Passed!");
+    assert.ok(!_.isEmpty(window.editor.body_placeholder), "Passed!");
+    assert.ok(!_.isEmpty(window.editor.embed_placeholder), "Passed!");
+    assert.ok(!_.isEmpty(window.editor.extract_placeholder), "Passed!");
+    assert.ok(!_.isEmpty(window.editor.upload_url), "Passed!");
+    assert.ok(!_.isEmpty(window.editor.oembed_url), "Passed!");
+    return assert.ok(!_.isEmpty(window.editor.extract_url), "Passed!");
+  });
+
+  QUnit.test("should init current_editor", function(assert) {
+    assert.ok(!_.isEmpty(window.current_editor), "Passed!");
+    assert.ok(_.isObject(window.current_editor.tooltip_view), "Passed!");
+    return assert.ok(_.isObject(window.current_editor.editor_menu), "Passed!");
+  });
+
+  QUnit.test("should build tooltip & menu", function(assert) {
+    assert.ok(!_.isEmpty($(".inlineTooltip2")), "Passed!");
+    return assert.ok(!_.isEmpty($("#editor-menu")), "Passed!");
+  });
+
+  QUnit.test("should display placeholders when empty content", function(assert) {
+    return assert.ok($("span.defaultValue").length === 2, "Passed!");
+  });
+
+}).call(this);
---
+++ source/javascripts/all.js
@@ -0,0 +1,7 @@
+//= require jquery/dist/jquery.js
+//= require sanitize.js/lib/sanitize.js
+//= require underscore/underscore.js
+//= require backbone/backbone.js
+
+//= require dante/dante
+
